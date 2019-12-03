9ecc5438a8ef3b3d6e197f3a572cd79957b6e90a
Kristoffer Katra <kristofferkatra@gmail.com>
2019-12-03 16:02:59 +0100

    added things to get shit to work hello

--- backend-api/src/main/java/com/movienights/api/configs/MyWebSecurityConfig.java
+++ backend-api/src/main/java/com/movienights/api/configs/MyWebSecurityConfig.java
@@ -1,11 +1,18 @@
 package com.movienights.api.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    MyUserDetailService userDetailService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -24,5 +31,12 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().logout().permitAll().logoutSuccessUrl("/")
                 .and().httpBasic()
                 .and().csrf().disable();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailService)
+                .passwordEncoder(userDetailService.getEncoder());
     }
 }
