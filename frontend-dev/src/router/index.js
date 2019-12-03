868c632922b0031a8e3aff067d38e64ba2e61666
Unknown <filip98@spray.se>
2019-12-03 15:45:42 +0100

    router

--- frontend-dev/src/router/index.js
+++ frontend-dev/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,15 +7,37 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: () => import('@/views/Home.vue')
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('@/views/About.vue')
+  },
+  {
+    path:'/login',
+    name: 'login',
+    component: () => import('@/views/Login.vue')
+  },
+  {
+    path:'/register',
+    name: 'register',
+    component: () => import('@/views/Register.vue')
+  },
+  {
+    path:'/search',
+    name: 'search',
+    component: () => import('@/views/Search.vue')
+  },
+  {
+    path:'/movie',
+    name: 'movie',
+    component: () => import('@/views/Movie.vue')
+  },
+  {
+    path:'/createEvent',
+    name: 'createEvent',
+    component: () => import('@/views/CreateEvent.vue')
   }
 ]
 
