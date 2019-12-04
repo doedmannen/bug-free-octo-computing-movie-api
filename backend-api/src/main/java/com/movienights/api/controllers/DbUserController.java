d966d36e0af552e205f484b873b69e619cce3cf5
Johan Schultze <Johan.schultze@gmail.com>
2019-12-04 10:39:13 +0100

    Get all users and get one specific user

--- backend-api/src/main/java/com/movienights/api/controllers/DbUserController.java
+++ backend-api/src/main/java/com/movienights/api/controllers/DbUserController.java
@@ -1,4 +1,52 @@
 package com.movienights.api.controllers;
 
+
+import com.movienights.api.entities.DbUser;
+import com.movienights.api.repos.DbUserRepo;
+import com.movienights.api.services.DbUserService;
+import org.apache.coyote.Response;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/users")
 public class DbUserController {
+
+    @Autowired
+    DbUserService dbUserService;
+
+    @GetMapping
+    ResponseEntity<List<DbUser>> getAllUsers() {
+        List users = dbUserService.getAllUsers();
+
+        if (users != null) {
+            if (users.size() != 0) {
+                return new ResponseEntity<>(users, HttpStatus.OK);
+            } else {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No users found");
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @GetMapping("{id}")
+    ResponseEntity<DbUser> getOneUser(@PathVariable String id) {
+        DbUser user = dbUserService.getOneUser(id);
+
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No user with that id was found");
+        }
+    }
+
 }
--- backend-api/src/main/java/com/movienights/api/services/DbUserService.java
+++ backend-api/src/main/java/com/movienights/api/services/DbUserService.java
@@ -1,4 +1,31 @@
 package com.movienights.api.services;
 
+import com.movienights.api.entities.DbUser;
+import com.movienights.api.repos.DbUserRepo;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class DbUserService {
+
+    @Autowired
+    DbUserRepo userRepo;
+
+    public List<DbUser> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    public DbUser getOneUser(String id) {
+        try {
+            ObjectId _id = new ObjectId(id);
+            return userRepo.findDbUserById(_id);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+
 }
