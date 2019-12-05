94ec30a857e561beee1114d305125e855acd64c7
Johan Schultze <Johan.schultze@gmail.com>
2019-12-05 16:30:43 +0100

    Stepper wip

--- frontend-dev/src/views/Register.vue
+++ frontend-dev/src/views/Register.vue
@@ -1,26 +1,60 @@
 <template>
-  <div class="register">
-    <h1>This is the register page</h1>
-    <v-form v-model="valid">
-      <v-container>
-        <v-row>
-          <v-col cols="12" md="12">
-            <v-text-field
-              v-model="username"
-              :rules="nameRules"
-              :counter="15"
-              label="Username"
-              required
-            ></v-text-field>
-          </v-col>
+  <div class="register d-flex justify-center">
+    <v-stepper class="stepper my-5 col-10" v-model="e1">
+      <v-stepper-header>
+        <v-stepper-step :complete="e1 > 1" step="1">User details</v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step :complete="e1 > 2" step="2">Link your email</v-stepper-step>
+      </v-stepper-header>
 
-          <v-col cols="12" md="12">
-            <v-text-field v-model="password" :rules="passwordRules" label="Password" required></v-text-field>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-form>
-    <v-btn @click="registerUser">Register</v-btn>
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          <v-card class="mb-12" color="grey lighten-4" height="200px">
+            <v-form v-model="valid">
+              <v-container class="py-0">
+                <v-row>
+                  <v-col cols="12" md="12">
+                    <v-text-field
+
+                      v-model="username"
+                      :rules="nameRules"
+                      :counter="15"
+                      label="Username"
+                      required
+                    ></v-text-field>
+                  </v-col>
+
+                  <v-col cols="12" md="12">
+                    <v-text-field
+                      v-model="password"
+                      :rules="passwordRules"
+                      label="Password"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-card>
+          <v-btn color="primary" @click="e1 = 2">Continue</v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content step="2">
+          <v-card class="mb-12" color="grey lighten-4" height="200px">
+            <h1>waooo</h1>
+          </v-card>
+          <v-btn color="primary" @click="e1 = 1">Back</v-btn>
+          <v-btn @click="registerUser">Register</v-btn>
+        </v-stepper-content>
+        <v-stepper-content step="3">
+          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+          <v-btn color="primary" @click="e1 = 1">Continue</v-btn>
+
+          <v-btn text>Cancel</v-btn>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
   </div>
 </template>
 
@@ -34,6 +68,7 @@ export default {
   },
 
   data: () => ({
+    e1: 0,
     valid: false,
     username: "",
     password: "",
@@ -45,29 +80,30 @@ export default {
     passwordRules: [
       v => !!v || "Password is required",
       v => v.length >= 5 || "Password must be atleast 5 characters"
-    ],
-
-  })
-  ,
-
+    ]
+  }),
   methods: {
-
     async registerUser() {
-      let result = await fetch('api/users', {
-        method: 'POST',
-        headers: {'Content-type' : 'application/json'},
-        body: JSON.stringify({username: this.username, password: this.password})
-      })
+      let result = await fetch("api/users/", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({
+          username: this.username,
+          password: this.password
+        })
+      });
 
-    result = await result.json()
+      result = await result.json();
 
-    console.log(result)
-
+      console.log(result);
     }
-
   }
 };
 </script>
 
 <style scoped>
+/* .register{
+  justify-content: center;
+  display: flex;
+} */
 </style>
