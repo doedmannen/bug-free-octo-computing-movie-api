ac8b335391f46a572a7da4e4cc861227588c5fa2
Kristoffer Katra <kristofferkatra@gmail.com>
2019-12-10 16:11:41 +0100

    added UUID to jwt and fixed destroy all jwts

--- backend-api/src/main/java/com/movienights/api/services/AuthService.java
+++ backend-api/src/main/java/com/movienights/api/services/AuthService.java
@@ -6,6 +6,7 @@ import com.movienights.api.entities.DbUser;
 import com.movienights.api.exceptions.CustomException;
 import com.movienights.api.repos.DbUserRepo;
 import com.movienights.api.tokenproviders.JwtTokenProvider;
+import com.movienights.api.watchlists.JwtWatchList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,10 +31,14 @@ public class AuthService {
     @Autowired
     DbUserRepo userRepo;
 
+    @Autowired
+    DbUserService userService;
+
     public TokenResponse login(String username, String password){
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-            return new TokenResponse(tokenProvider.createToken(username, userRepo.findDistinctFirstByUsernameIgnoreCase(username).getRoles()));
+            DbUser user = userRepo.findDistinctFirstByUsernameIgnoreCase(username);
+            return new TokenResponse(tokenProvider.createToken(user.getUsername(), user.getJwtSalt().toString(), user.getRoles()));
         } catch (AuthenticationException e) {
             throw new CustomException("Invalid username/password supplied", HttpStatus.UNPROCESSABLE_ENTITY);
         }
@@ -42,14 +47,15 @@ public class AuthService {
     public void invalidateAllUserTokens(HttpServletRequest request){
         if(!tokenProvider.validateToken(tokenProvider.resolveToken(request)))
             return;
+        userService.revokeUserJwt(tokenProvider.getUsername(request));
+        JwtWatchList.getInstance().watchFor(tokenProvider.getUsername(request));
     }
 
     public ResponseEntity<TokenResponse> refresh(HttpServletRequest request){
         String token = tokenProvider.resolveToken(request);
-        String username = tokenProvider.getUserNameForTokenRenewal(token);
-        DbUser user = userRepo.findDistinctFirstByUsernameIgnoreCase(username);
+        DbUser user = tokenProvider.getUserForTokenRenewal(token);
         if(user != null) {
-            return new ResponseEntity<>(new TokenResponse(tokenProvider.createToken(user.getUsername(), user.getRoles())), HttpStatus.OK);
+            return new ResponseEntity<>(new TokenResponse(tokenProvider.createToken(user.getUsername(), user.getJwtSalt().toString(), user.getRoles())), HttpStatus.OK);
         }
         return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
     }
