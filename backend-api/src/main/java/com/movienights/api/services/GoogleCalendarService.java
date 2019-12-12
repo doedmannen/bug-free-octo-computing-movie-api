6e07072bb72c5c2413d6d93fb021629d1d3f9ceb
Kristoffer Katra <kristofferkatra@gmail.com>
2019-12-12 14:40:01 +0100

    build of new methods and refactor

--- backend-api/src/main/java/com/movienights/api/services/GoogleCalendarService.java
+++ backend-api/src/main/java/com/movienights/api/services/GoogleCalendarService.java
@@ -5,21 +5,24 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.FreeBusyRequest;
-import com.google.api.services.calendar.model.FreeBusyRequestItem;
-import com.google.api.services.calendar.model.FreeBusyResponse;
+import com.google.api.services.calendar.model.*;
+import com.movienights.api.dto.BusyInterval;
+import com.movienights.api.dto.EventCollector;
 import com.movienights.api.dto.EventSuggestion;
 import com.movienights.api.entities.DbUser;
+import com.movienights.api.entities.Movie;
+import com.movienights.api.exceptions.CustomException;
 import com.movienights.api.repos.DbUserRepo;
 import com.movienights.api.repos.MovieRepo;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.time.*;
+import java.util.*;
 
 @Service
 public class GoogleCalendarService {
@@ -30,17 +33,28 @@ public class GoogleCalendarService {
     @Autowired
     MovieRepo movieRepo;
 
+    @Autowired
+    DbUserService dbUserService;
 
-    public Set<EventSuggestion> getEventSuggestions(List<String> usernames, String movieId) {
+
+
+    public List<EventSuggestion> getEventSuggestions(List<String> usernames, String movieId) {
+        // Renew all access tokens
+        usernames.forEach(s -> dbUserService.refreshAccessToken(s));
+
         Set<DbUser> users = getUsersFromDb(usernames);
-        Set<EventSuggestion> eventSuggestions = new HashSet<EventSuggestion>();
-        //Set<>
-        if(!users.isEmpty()){
-            DateTime starting = new DateTime(java.util.Calendar.getInstance().getTimeInMillis());
-            DateTime ending = new DateTime(java.util.Calendar.getInstance().getTimeInMillis() + 31556926000L);
+        Set<Calendar> calendars = getUsersCalendars(users);
+        Set<FreeBusyResponse> freeBusyResponses = getFreeBusyResponses(calendars);
+        Optional<Movie> movie = movieRepo.findById(new ObjectId(movieId));
+        EventCollector eventCollector = getEventCollector(freeBusyResponses);
 
+        if(movie.isEmpty()){
+            throw new CustomException("Invalid movie", HttpStatus.NOT_FOUND);
         }
-
+        List<EventSuggestion> eventSuggestions = produceEventSuggestions(eventCollector, movie.get());
+        if(eventSuggestions.isEmpty()){
+            throw new CustomException("Nobody has time", HttpStatus.NOT_FOUND);
+        }
         return eventSuggestions;
     }
 
@@ -54,7 +68,7 @@ public class GoogleCalendarService {
     }
 
     @SuppressWarnings("deprecation")
-    private Set<Calendar> getUsersCalendars(Set<DbUser> users){
+    private Set<Calendar> getUsersCalendars(Set<DbUser> users) {
         Set<Calendar> calendars = new HashSet<>();
         users.forEach(user -> {
             GoogleCredential credential = new GoogleCredential().setAccessToken(user.getAccessToken());
@@ -65,19 +79,56 @@ public class GoogleCalendarService {
         return calendars;
     }
 
+    private Set<FreeBusyResponse> getFreeBusyResponses(Set<Calendar> calendars){
+        Set<FreeBusyResponse> responses = new HashSet<>();
+        DateTime starting = new DateTime(java.util.Calendar.getInstance().getTimeInMillis());
+        DateTime ending = new DateTime(java.util.Calendar.getInstance().getTimeInMillis() + 31556926000L);
+        try {
+            for(Calendar calendar : calendars){
+                FreeBusyRequest request = new FreeBusyRequest();
+                request.setTimeMin(starting);
+                request.setTimeMax(ending);
+                request.setItems(List.of(new FreeBusyRequestItem().setId("primary")));
+                responses.add(calendar.freebusy().query(request).execute());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Set.of();
+        }
+        return responses;
+    }
 
+    private EventCollector getEventCollector(Set<FreeBusyResponse> freeBusyResponses){
+        EventCollector eventCollector = new EventCollector();
 
+        freeBusyResponses.forEach(freeBusyResponse -> {
+            freeBusyResponse.getCalendars().values()
+                    .forEach(freeBusyCalendar -> freeBusyCalendar.getBusy()
+                            .forEach(busy -> eventCollector.addBusyInterval(new BusyInterval(busy.getStart().getValue(), busy.getEnd().getValue()))));
+        });
+        return eventCollector;
+    }
 
-//        try {
-//        FreeBusyRequest request = new FreeBusyRequest();
-//        request.setTimeMin(now);
-//        request.setTimeMax(then);
-//        request.setItems(List.of(new FreeBusyRequestItem().setId("primary")));
-//        FreeBusyResponse response = calendar.freebusy().query(request).execute();
-//
-//        return response;
-//    } catch (
-//    IOException e) {
-//        e.printStackTrace();
-//    }
+    private List<EventSuggestion> produceEventSuggestions(EventCollector eventCollector, Movie movie){
+        long movieLengthMillis;
+        List<EventSuggestion> suggestions = new ArrayList<>();
+        try{
+            movieLengthMillis = Long.parseLong(movie.getRuntime().replaceAll("[^0-9]","")) * 60000L;
+        } catch(Exception e){
+            throw new CustomException("The specific movie has no runtime", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        LocalDateTime current = LocalDateTime.now();
+        ZonedDateTime timeSuggestor = ZonedDateTime.of(current.getYear(), current.getMonthValue(), current.getDayOfMonth(), 19, 0, 0, 0, ZoneId.systemDefault());
+
+        for(int i = 0; i < 100 || suggestions.size() > 20; i++) {
+            timeSuggestor.plusDays(1);
+            long start = timeSuggestor.toEpochSecond() * 1000L;
+            long end = start + movieLengthMillis;
+            if (eventCollector.isEventSuggestionAvailable(start, end)) {
+                suggestions.add(new EventSuggestion(start, end));
+            }
+        }
+        return suggestions;
+    }
+
 }
